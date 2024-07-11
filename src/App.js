46839082b8ed8300da959f76a24a6ec99a224048
Lokie-ree <165349722+Lokie-ree@users.noreply.github.com>
2024-07-11 15:26:43 -0500

    added css, title, search bar/icon, and API call

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import { useState } from 'react'
+import React, { useEffect } from 'react'
+
 import './App.css';
+import SearchIcon from './search.svg';
 
+const API_URL = "http://www.omdbapi.com?apikey=bbf77501"
 
-const App = (props) => {
-  const [counter, setCounter] = useState(10)
+const App = () => {
+  
+  const searchMovies = async (title) => {
+    const response = await fetch (`${API_URL}&s=${title}`)
+    const data = await response.json();
+
+    console.log(data.Search);
+  }
+  
+  useEffect(() => {
+    searchMovies('Spiderman');
+  }, []);
 
   return (
-    <div className="App">
-      <button onClick={() => {setCounter(counter - 1)}}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={() => {setCounter(counter + 1)}}>+</button>
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input 
+          placeholder="Search for movies"
+          value="Superman"
+          onChange={() => {}}
+        />
+        <img 
+          src={SearchIcon}
+          alt="search"
+          onClick={() => {}}
+        />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
